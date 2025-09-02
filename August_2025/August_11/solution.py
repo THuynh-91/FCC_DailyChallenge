c2ae3d2fdfe5cb07c3881b0133f104f105072c10
THuynh-91 <triqhuynh91@gmail.com>
2025-09-02 18:06:55 -0400

    Update solution.py

--- August_2025/August_11/solution.py
+++ August_2025/August_11/solution.py
@@ -1,13 +1,10 @@
 def is_balanced(s):
-
     s = s.lower()
     v = 'aeiou'
 
-
     lower = s[0:len(s)//2]
     upper = s[(len(s) +1)//2:]
-
-
+    
     return sum(c in v for c in lower) == sum(c in v for c in upper)
   
 print(is_balanced("racecar")) # -> True
